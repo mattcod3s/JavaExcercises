98db2ea6bbbd69613a7ad8cccb7d5455e01b915a
MattCod3s <matthew.kostka08@gmail.com>
2021-04-05 00:50:25 +0100

    all files added

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,64 +1,16 @@
 package ie.tudublin;
 
-public class Main
-{
-    public void catsAndDogs()
-    {
-        System.out.println("Hello world");
+public class Main {
 
-        Animal misty = new Dog("Misty");
-
-        Animal topCat = new Cat("TopCat");
-
-        System.out.println(misty);
-        System.out.println(topCat);
-
-        misty = topCat;
-
-        topCat.setName("Garfield");
-
-        System.out.println(misty);
-        System.out.println(topCat);
-
-        // What will get printed out??
-
-        // 1. topcat, Garfield
-        // 2. garfield, Garfield 
-
-        Cat ginger = new Cat("Ginger");
-        while(ginger.getNumLives() > 0)
-        {
-            ginger.kill();
-        }
-        ginger.kill();
-    }
-
-    public void helloProcessing1()
+    public void vizualizer()
     {
         String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing1());
+        processing.core.PApplet.runSketch( a, new Vizualizer());
     }
-
-    public void helloProcessing2()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing2());
-    }
-
-    public void bugZap()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
-    }
-
-    public void loops()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
-    }
-    public static void main(String[] args)
+    
+        public static void main(String[] args)
     {
         Main main = new Main();
-        main.loops();
+        main.vizualizer();
     }
 } 
